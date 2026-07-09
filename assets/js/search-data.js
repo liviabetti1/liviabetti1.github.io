d07e584e0a1202d004d3b9932f601af82a12bbdd
liviabetti1 <90067571+liviabetti1@users.noreply.github.com>
2026-07-09 23:02:45 +0000

    Deploying to gh-pages from @ liviabetti1/liviabetti1.github.io@70c5520719307c2c6e27f798bf3a88f45951729c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "nav-publications",
           title: "publications",
           description: "",
           section: "Navigation",
@@ -35,8 +42,14 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-new-preprint-ot-on-the-map-on-quantifying-domain-shifts-in-geographic-space-is-out-on-arxiv",
+          title: 'New preprint: OT on the Map, on quantifying domain shifts in geographic space,...',
+          description: "",
+          section: "News",},{id: "news-new-preprint-what-s-in-an-earth-embedding-an-explainability-analysis-of-location-encoders-is-out-on-arxiv",
+          title: 'New preprint: What’s in an Earth Embedding?, an explainability analysis of location encoders,...',
+          description: "",
+          section: "News",},{id: "news-our-paper-mapping-on-a-budget-was-accepted-to-aaai-2026",
+          title: 'Our paper Mapping on a Budget was accepted to AAAI 2026!',
           description: "",
           section: "News",},{id: "projects-boulder-trailhead-camera-map",
           title: 'Boulder Trailhead Camera Map',
@@ -49,6 +62,13 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/spatial-data-collection/";
             },},{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=XMFF300AAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
